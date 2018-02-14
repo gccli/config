8c6dd9565fac0e57ce5ec4b9d1628dd1ac97c256
Jing Li <lijing@example.com>
2018-02-14 14:55:13 +0000

    Add files

--- config-git.sh
+++ config-git.sh
@@ -6,7 +6,7 @@ url=$(git remote -v |sed -n '1p'|awk '{ print $2 }')
 echo $url | egrep '^https' >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "config git..."
-    git config credential.helper 'cache --timeout=86400'
+    git config credential.helper 'cache --timeout=864000'
     git config credential.$url gccli
     git config --local -l
 fi
