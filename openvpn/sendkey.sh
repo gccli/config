62edcd3d78aefdb60223b8cafb744a988a8a0432
Jing Li <lijing@example.com>
2018-03-14 16:10:17 +0000

    add sendkey.sh scripts

---
+++ openvpn/sendkey.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name=$1
+if [ "${name}x" == "x" ]; then
+    echo "Usage: $0 name"
+    exit 0
+fi
+
+tempdir=$(mktemp -d)
+
+rm -f $name.tar.gz
+cp -f easyrsa/pki/ca.crt easyrsa/pki/issued/${name}.crt easyrsa/pki/private/${name}.key $tempdir
+tar -C $tempdir -czf $name.tar.gz .
+tar -tzf $name.tar.gz
