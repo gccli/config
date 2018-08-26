147021a21401d227be7621f35fe167106fe9d9fa
Jing Li <lijing@example.com>
2018-08-26 16:29:59 +0800

    install pydevel

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+yum install -y python-devel
 python download.py
 python get-pip.py -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -U -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
