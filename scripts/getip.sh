f9e053e35e236e693c2789a72e019350e53bd31a
Jing Li <lijing@example.com>
2018-09-02 15:35:40 +0000

    add getip.sh

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-which dig >/dev/null 2>&1 || yum install -y bind-utils >/dev/null 2>&1
+test -f /usr/bin/dig || yum install -y bind-utils >/dev/null 2>&1
 dig @resolver1.opendns.com -t A -4 myip.opendns.com +short
