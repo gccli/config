f529d289f3fc865589c4598f3ec8afda221570ef
Jing Li <lijing@example.com>
2018-09-03 06:07:34 +0000

    modify scripts

--- cloudinit.sh
+++ cloudinit.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+# curl -L git.io/cloudinit|bash
+
 yum install -y emacs-nox git tcpdump lrzsz
-[ ! -d config ] && git clone https://github.com/gccli/config.git
+[ ! -d config ] && git clone https://github.com/gccli/config.git && cd config && ./config.sh
