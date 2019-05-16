a1d2efa6e23d640a0598ab31e0fdb34309e1e7e7
Jing Li <lijing@example.com>
2019-05-16 20:47:51 +0800

    modify ns-cleanup

--- bash/.functions.d/namespace.sh
+++ bash/.functions.d/namespace.sh
@@ -1,18 +1,18 @@
 ns-create () {
-    NAME=$1
-    IP=$2
+    local name=$1
+    local cidr=$2
 
-    sudo ip netns add ${NAME}
-    sudo ip link add dev veth-${NAME} type veth peer name veth0 netns $NAME
-    sudo ip link set dev veth-${NAME} up
-    sudo ip netns exec ${NAME} ip link set dev veth0 up
-    if [ -n "$IP" ]; then
-        if ! echo $IP | egrep '/[0-9]+'; then
-           IP=$IP/24
+    ip netns add ${name}
+    ip link add dev veth-${name} type veth peer name veth0 netns $name
+    ip link set dev veth-${name} up
+    ip netns exec ${name} ip link set dev veth0 up
+    if [ -n "$cidr" ]; then
+        if ! echo $cidr | egrep '/[0-9]+'; then
+           cidr=$cidr/24
         fi
-        sudo ip netns exec ${NAME} ip addr add dev veth0 $IP
+        ip netns exec ${name} ip addr add dev veth0 $cidr
     fi
-    sudo ip netns exec ${NAME} ip link set dev lo up
+    ip netns exec ${name} ip link set dev lo up
 }
 
 ns-create2 () {
@@ -31,52 +31,59 @@ ns-create2 () {
 ns-ex () {
     NS=$(ip netns | egrep "\(id: $1\)")
     if [ $? -eq 0 ]; then
-        NAME=$(echo $NS | awk '{ print $1 }')
+        name=$(echo $NS | awk '{ print $1 }')
     else
-        NAME=$1
+        name=$1
     fi
     shift
-    sudo ip netns exec $NAME $@
+    ip netns exec $name $@
 }
 
 ns-cleanup () {
-    for NETNS in $(sudo ip netns list | awk '{print $1}'); do
+    for NETNS in $(ip netns list | awk '{print $1}'); do
         [ -n "$NETNS" ] || continue
-        NAME=${NETNS}
-        if [ -f "/run/dhclient-${NAME}.pid" ]; then
+        name=${NETNS}
+        if [ -f "/run/dhclient-${name}.pid" ]; then
             # Stop dhclient
-            sudo pkill -F "/run/dhclient-${NAME}.pid"
+            pkill -F "/run/dhclient-${name}.pid"
         fi
-        if [ -f "/run/iperf3-${NAME}.pid" ]; then
+        if [ -f "/run/iperf3-${name}.pid" ]; then
             # Stop iperf3
-            sudo pkill -F "/run/iperf3-${NAME}.pid"
+            pkill -F "/run/iperf3-${name}.pid"
         fi
-        if [ -f "/run/bird-${NAME}.pid" ]; then
+        if [ -f "/run/bird-${name}.pid" ]; then
             # Stop bird
-            sudo pkill -F "/run/bird-${NAME}.pid"
+            pkill -F "/run/bird-${name}.pid"
         fi
-        # Remove netns and veth pair
-        sudo ip link delete veth-${NAME}
-        sudo ip netns delete $NETNS
+
+        ip netns delete $NETNS
     done
     for DNSMASQ in /run/dnsmasq-vlan*.pid; do
         [ -e "$DNSMASQ" ] || continue
         # Stop dnsmasq
-        sudo pkill -F "${DNSMASQ}"
+        pkill -F "${DNSMASQ}"
     done
-    # Remove faucet dataplane connection
-    sudo ip link delete veth-faucet 2>/dev/null || true
+
+    # Remove veth pair
+    for ifname in $(ls /sys/class/net); do
+        if echo $ifname | egrep '^veth'; then
+            ip link delete $ifname
+        fi
+    done
+
     # Remove openvswitch bridge
-    sudo ovs-vsctl del-br br0
+    for br in $(ovs-vsctl list-br); do
+        ovs-vsctl del-br $br
+    done
 }
 
 ns-add-vlanif () {
-     NAME=$1
-     NETNS=$1
-     IP=$2
-     VLAN=$3
-     sudo ip netns exec ${NETNS} ip link add link veth0 name veth0.${VLAN} type vlan id $VLAN
-     sudo ip netns exec ${NETNS} ip link set dev veth0.${VLAN} up
-     sudo ip netns exec ${NETNS} ip addr flush dev veth0
-     sudo ip netns exec ${NETNS} ip addr add dev veth0.${VLAN} $IP
+    local netns=$1
+    local ip=$2
+    local vlan=$3
+
+    ip netns exec ${netns} ip link add link veth0 name veth0.${vlan} type vlan id $vlan
+    ip netns exec ${netns} ip link set dev veth0.${vlan} up
+    ip netns exec ${netns} ip addr flush dev veth0
+    ip netns exec ${netns} ip addr add dev veth0.${vlan} $ip
 }
