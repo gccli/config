a10e6400fb6ae1ae827ca26239034eccad1eb2cd
Jing Li <lijing@example.com>
2019-10-04 23:36:16 +0000

    fix

--- scripts/allow.sh
+++ scripts/allow.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-host=$1
-if [ -z "$host" ]; then
-    echo "Usage: $0 host" && exit 1
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <host> ..."
+    echo "  e.g. $0 223.72.51.39 93.184.216.34"
+    exit 1
 fi
 port=$(egrep '^Port' /etc/ssh/sshd_config |awk '{print $2}')
 [ -z "$port" ] && port=22
 
-iptables -I INPUT -p tcp -s $host --dport 20212 -j ACCEPT
-iptables -I INPUT 2 -p tcp -s 0.0.0.0/0 --dport 20212 -j DROP
+iptables -F INPUT
+linenum=1
+for host in $@; do
+    iptables -I INPUT -p tcp -s $host --dport $port -j ACCEPT
+    linenum=$(($linenum+1))
+done
+
+iptables -I INPUT $linenum -p tcp -s 0.0.0.0/0 --dport $port -j DROP
 iptables -L INPUT -n -v --line-numbers
