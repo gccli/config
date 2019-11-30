21d74f8e07905b36b1d99188b12ebf9059b5542a
Jing Li <lijing@example.com>
2019-11-30 06:27:02 +0000

    add script

--- scripts/allow.sh
+++ scripts/allow.sh
@@ -15,5 +15,13 @@ for host in $@; do
     linenum=$(($linenum+1))
 done
 
+for host in $@; do
+    iptables -I INPUT -p udp -s $host -j ACCEPT
+    linenum=$(($linenum+1))
+done
+
+
 iptables -I INPUT $linenum -p tcp -s 0.0.0.0/0 --dport $port -j DROP
+linenum=$(($linenum+1))
+#iptables -I INPUT $linenum -p udp -s 0.0.0.0/0 -j DROP
 iptables -L INPUT -n -v --line-numbers
