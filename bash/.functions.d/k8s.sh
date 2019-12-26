fed62890ae852e2f078f983a3600a23d5ea787d4
Jing Li <lijing@example.com>
2019-12-26 23:00:57 +0800

    add ksys

---
+++ bash/.functions.d/k8s.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias k='kubectl'
+alias ksys='kubectl -n kube-system'
+alias kall='kubectl get po -A'
+alias kallw='kubectl get po -A -o wide'
+alias kexec='kubectl exec -it'
+alias kdpo='kubectl describe po'
+alias klog='kubectl logs'
