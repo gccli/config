68400611b349a9f02275fb3765b43de9837b471b
Jing Li <lijing@example.com>
2020-05-17 10:30:06 +0800

    scripts

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -3,7 +3,8 @@
 GOVERSION=1.14.3
 
 #GOURL=https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz
-GOURL=https://gomirrors.org/dl/go/go${GOVERSION}.linux-amd64.tar.gz
+#GOURL=https://gomirrors.org/dl/go/go${GOVERSION}.linux-amd64.tar.gz
+GOURL=file:///opt/www/pub/go${GOVERSION}.linux-amd64.tar.gz
 GOROOT=/usr/local/go
 GOBASE=$(dirname $GOROOT)
 #GOPROXY=https://mirrors.aliyun.com/goproxy/
@@ -13,6 +14,7 @@ rm -rf $GOROOT
 echo "curl -sL ${PROXY_OPTS} $GOURL | tar -C $GOBASE -xzv"
 curl -sL ${PROXY_OPTS} $GOURL | tar -C $GOBASE -xzv
 
+# REFERENCE: https://goproxy.io/zh/
 cat >/etc/profile.d/golang.sh <<EOF
 export PATH=$PATH:$GOROOT/bin
 export GOPATH=/opt/go
@@ -20,10 +22,11 @@ export GOPROXY=$GOPROXY
 export GOSUMDB=gosum.io+ce6e7565+AY5qEHUk/qmHc5btzW45JVoENfazw8LielDsaI+lEbq6
 EOF
 
-# REFERENCE: https://goproxy.io/zh/
+echo
+source /etc/profile.d/golang.sh
+
+go version
+go env
+
 echo
 echo "Run [source /etc/profile.d/golang.sh] load golang environemnt"
-
-source /etc/profile.d/golang.sh
-go version
-go env | egrep '^GO'
