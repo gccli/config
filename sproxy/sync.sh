ca6fdbed69b88a7bc8f490a6a66a092eafc05ae8
Jing Li <lijing@example.com>
2020-07-25 09:58:27 +0800

    sync

--- sproxy/sync.sh
+++ sproxy/sync.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ip=$1
-if [ "${ip}x" == "x" ]; then
-    echo "Usage $0 <ip>"
+domain=$2
+if [ $# != 2 ]; then
+    echo "Usage $0 <ip> <domain>"
     exit 0
 fi
 
-scp -p /etc/pki/tls/certs/inetlinux.com.crt   $ip:/etc/pki/tls/certs/inetlinux.com.crt
-scp -p /etc/pki/tls/private/inetlinux.com.key $ip:/etc/pki/tls/private/inetlinux.com.key
+scp -p ~/git/certs/pki/issued/${domain}.crt   $ip:/etc/pki/tls/certs/${domain}.crt
+scp -p ~/git/certs/pki//private/${domain}.key $ip:/etc/pki/tls/private/${domain}.key
 
 rsync -av ../sproxy $ip:~/
 ssh $ip yum install -y epel-release
